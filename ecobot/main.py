830e0b699ab462913dc420a96aa326e4d6e69901
FastNaLast <daniil.mi0126@gmail.com>
2025-06-11 18:56:02 +0500

    Update main.py

--- ecobot/main.py
+++ ecobot/main.py
@@ -2,7 +2,7 @@ import telebot
 import requests
 import random, os
 import time 
-bot = telebot.TeleBot("code")
+bot = telebot.TeleBot("")
 img=os.listdir("./photo")
 
 @bot.message_handler(commands=['start'])
@@ -17,21 +17,21 @@ def sort(message):
     time.sleep(5)
     bot.reply_to(message,"1. Пластик Что сюда относится: \n Бутылки от воды, напитков, молока (с маркировкой PET, PE-HD, PP) \n Упаковка от продуктов (контейнеры, пакеты, плёнка) \n"
                  "Как подготовить: \n Снимите крышки (их можно сдать отдельно, если они металлические). \n Ополосните от остатков пищи. \n Сплющите бутылки и упаковку, чтобы занимали меньше места. \n Что нельзя: \n Грязную упаковку с остатками еды Игрушки, трубы, CD-диски (это не перерабатываемый пластик)")
-    with open(f"./photo/{random.choice(plastik)}","rb") as f:
+    with open("./photo/plastik.jpg","rb") as f:
                 bot.send_photo(message.chat.id, f)
     time.sleep(20)
     bot.reply_to(message,"2. Бумага и картон Что сюда относится: \n Газеты, журналы, офисная бумага \n Коробки от товаров, упаковочный картон \n Бумажные пакеты, книги (без обложек) \n "
                  "Как подготовить:\n Удалите скотч, скрепки, плёнку.\n Расправьте коробки, чтобы экономить место. \n Жирную или мокрую бумагу выбрасывайте в общий мусор \n Что нельзя: \n Чеки, ламинированную бумагу, салфетки, обои")
-    with open(f"./photo/{random.choice(paper)}","rb") as f:
+    with open("./photo/paper.webp","rb") as f:
                 bot.send_photo(message.chat.id, f)
     time.sleep(20)
     bot.reply_to(message,"3. Стекло \n Что сюда относится: \n Бутылки от напитков, банки от консервов \n Стеклянная тара от косметики (без дозаторов) \n "
                 "Как подготовить: \n Промойте от остатков содержимого. \n Снимите крышки (металлические можно сдать в металл). \n Не разбивайте стекло – это опасно для работников сортировки. \n Что нельзя: \n Лампочки \n Зеркала \n Хрусталь \n Керамику")
-    with open(f"./photo/{random.choice(steklo)}","rb") as f:
+    with open("./photo/steklo.webp","rb") as f:
                 bot.send_photo(message.chat.id, f)
     time.sleep(20)
     bot.reply_to(message,"4. Металл \n Что сюда относится: \n Алюминиевые и жестяные банки \n Крышки от банок и бутылок \n Фольга (если чистая) \n Как подготовить: \n Промойте банки от остатков пищи. \n По возможности сплющите. \n Что нельзя: \n Батарейки \n Баллончики из-под аэрозолей")
-    with open(f"./photo/{random.choice(metal)}","rb") as f:
+    with open("./photo/metal.png","rb") as f:
                 bot.send_photo(message.chat.id, f)
 
 
@@ -43,3 +43,6 @@ def echo_all(message):
     bot.reply_to(message, message.text)
 
 bot.polling()
+
+
+
